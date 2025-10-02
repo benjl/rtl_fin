a7934cadfd0949c11cddb39a03d66c373aeb7cf0
benjl <32750171+benjl@users.noreply.github.com>
2025-10-01 21:35:02 -0600

    Add a volume

--- test_bash.sh
+++ test_bash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --device /dev/bus/usb/003/002 -p 1190:1190 rtl_fin /bin/bash
+docker run -it --rm --device /dev/bus/usb/003/002 -p 1190:1190 -v ./output:/output rtl_fin /bin/bash
