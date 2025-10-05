00c22d35881dba839c169589f40a8013cb434144
benjl <32750171+benjl@users.noreply.github.com>
2025-10-04 18:53:05 -0600

    Pass whole bus bc it keeps changing and freq env var

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+cp /ezstream_cfg.xml /ezstream_freq.xml
+sed -i "s/{FREQUENCY}/$FREQUENCY/g" /ezstream_freq.xml
 icecast -c /etc/icecast2/icecast_cfg.xml &
-sleep 5 && rtl_fm -f 102.1M -M wbfm -E dc - | sox -t raw -r 32k -b 16 -c 1 -e signed-integer -V1 /dev/stdin -t mp3 - bandreject 10000 2000 | ezstream -c /ezstream_cfg.xml
+sleep 5 && rtl_fm -f $FREQUENCY -M wbfm -E dc - | sox -t raw -r 32k -b 16 -c 1 -e signed-integer -V1 /dev/stdin -t mp3 - bandreject 10000 2000 | ezstream -c /ezstream_freq.xml
