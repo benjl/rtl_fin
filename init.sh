1e6954c26613ee9484774541d579baa7a8a3dff9
benjl <32750171+benjl@users.noreply.github.com>
2025-10-20 22:06:31 -0600

    No oversampling

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 cp /ezstream_cfg.xml /ezstream_freq.xml
 sed -i "s/{FREQUENCY}/$FREQUENCY/g" /ezstream_freq.xml
 icecast -c /etc/icecast2/icecast_cfg.xml &
-sleep 5 && rtl_fm -f $FREQUENCY -M wbfm -E dc -g 30 - | sox -t raw -r 32k -b 16 -c 1 -e signed-integer -V1 /dev/stdin -t mp3 - lowpass 4000 | ezstream -c /ezstream_freq.xml
+sleep 5 && rtl_fm -f $FREQUENCY -M fm -s 170k -A fast -l 0 -E deemp -E dc -g 30 - | sox -t raw -r 170k -b 16 -c 1 -e signed-integer -V1 /dev/stdin -t mp3 -r 32k - lowpass 4000 | ezstream -c /ezstream_freq.xml
